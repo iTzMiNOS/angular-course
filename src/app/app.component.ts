669dd4db825fc71039364d2ef5454eb150530291
iTzMiNOS <amirerf1998@gmail.com>
2023-09-14 21:41:17 +0300

    modified:   src/app/app.component.html 	modified:   src/app/app.component.ts 	new file:   src/app/services/courses.service.spec.ts 	new file:   src/app/services/courses.service.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,8 @@ import {Course} from './model/course';
 import {CourseCardComponent} from './course-card/course-card.component';
 import {HighlightedDirective} from './directives/highlighted.directive';
 import {Observable} from 'rxjs';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { CoursesService } from './services/courses.service';
 
 @Component({
   selector: 'app-root',
@@ -12,14 +14,23 @@ import {Observable} from 'rxjs';
 })
 export class AppComponent implements OnInit {
 
+  courses$ : Observable<Course[]>;
 
-  courses = COURSES;
-
-  constructor() {
+  constructor(private coursesServices: CoursesService) {
 
   }
 
   ngOnInit() {
+
+    this.courses$ = this.coursesServices.loadCourses();
+
+  }
+
+  save(course:Course){
+    this.coursesServices.saveCourse(course)
+      .subscribe(
+        () => console.log('course saved')
+      );
   }
 
 
